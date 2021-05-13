b865f818a3e14707aef67cda59179fd16d075c0b
finlaywatling <finlaywatling@gmail.com>
2021-05-13 14:26:21 +0100

    javascript

--- assets/core.js
+++ assets/core.js
@@ -4,12 +4,17 @@ function init() {
 
 
 //alert("Javascript loaded");
+var submitBtn = document.querySelector('#submitbutton');
+
+submitBtn.addEventListener('click', submit);
+
+function submit(){
+
+console.log("hello world")
 
 
 
-
-
-
+}
 
 
 
