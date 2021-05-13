eecb3ac3f4ce859cb9a928d821a411e891c79998
finlaywatling <finlaywatling@gmail.com>
2021-05-13 15:21:54 +0100

    worked on javascript quiz

--- assets/core.js
+++ assets/core.js
@@ -7,19 +7,34 @@ function init() {
 var submitBtn = document.querySelector('#submitbutton');
 
 submitBtn.addEventListener('click', submit);
+var correct = 0;
+
 
 function submit(){
+var answers = document.querySelectorAll("#value");
+let output = document.querySelector("#output");
 
-console.log("hello world")
 
+for (let i = 0; i < answers.length; i++) {
+    if (answers[i].checked) {
+        
+        correct++
+
+        
+
+        
+        
+    }
 
 
 }
 
+console.log(correct);
 
 
 
 
 
 
-}
+
+}}
